1c4eb1017790e36f23c0105e48d942525b98fc2c
w311ang <30381083+w311ang@users.noreply.github.com>
2022-05-27 20:58:21 +0800

    Delete auth.py

--- auth.py
+++
@@ -1,9 +0,0 @@
-import json
-from pytools.pytools import pas
-from pytools.pytools import base64decode
-import os
-
-
-config=json.loads(base64decode(os.environ['config']))
-url=config['server']+':'+config['server_port']
-pas(url,os.environ['password'])
